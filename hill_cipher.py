6b7bca21e9862532597fa9db284fd47b6e2dadcb
rezaebadi <rezaaebadi1381@gmail.com>
2024-07-01 11:44:30 +0330

    decryption is done now

--- hill_cipher.py
+++ hill_cipher.py
@@ -34,7 +34,7 @@ def encrypt(text, key):
     if(text_len%key_len!=0):
         for h in range(text_len%key_len):
             text+='@'
-    for i in range(text_len//key_len):
+    for i in range(len(text)//key_len):
         vector = [ord(text[i*key_len+j]) - first_character for j in range(key_len)]
         for p in range(key_len):
          result=0
@@ -43,27 +43,69 @@ def encrypt(text, key):
          encrypted_text += chr((result%num_of_characters)+first_character)
     return encrypted_text
 
-# def decrypt(cipher_text, key):
-#     decrypted_text = ""
-#     n = len(key)
-#     det = round((key[0][0]*key[1][1]) - (key[0][1]*key[1][0]))
-#     det_inv = 0
-#     for i in range(26):
-#         if (det * i) % 26 == 1:
-#             det_inv = i
-#             break
-#     key_inv = [[(key[1][1]*det_inv) % 26, (-key[0][1]*det_inv) % 26], [(-key[1][0]*det_inv) % 26, (key[0][0]*det_inv) % 26]]
-#     for i in range(0, len(cipher_text), n):
-#         vector = [ord(cipher_text[i+j]) - 65 for j in range(n)]
-#         result = [(vector[j] * key_inv[j][0] + vector[(j+1)%n] * key_inv[(j+1)%n][0]) % 26 for j in range(n)]
-#         decrypted_text += ''.join([chr(result[j] + 65) for j in range(n)])
-#     return decrypted_text
 
+def mod_inverse(a, m):
+    for i in range(1, m):
+        if (a * i) % m == 1:
+            return i
+    return None
+
+def matrix_cofactor(matrix):
+    return [
+        [
+            ((-1) ** (i + j)) * determinant([row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])])
+            for j in range(len(matrix))
+        ]
+        for i in range(len(matrix))
+    ]
+
+def transpose(matrix):
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix))]
+
+def generate_invert_key(key):
+    det = round(determinant(key)) % num_of_characters
+    det_inv = mod_inverse(det, num_of_characters)
+    
+    if det_inv is None:
+        raise ValueError("The key matrix is not invertible modulo 26")
+    
+    cofactor_matrix = matrix_cofactor(key)
+    adj_matrix = transpose(cofactor_matrix)
+    
+    inv_key = [[(det_inv * adj_matrix[i][j]) % num_of_characters for j in range(len(key))] for i in range(len(key))]
+    
+    return inv_key
+
+def validate_decryption_input(text):
+    for char in text:
+        if not char.isalpha() and char != '@':
+            raise ValueError(f"Invalid character '{char}' found. Only alphabetic characters and '@' are allowed.")
+        
+def decrypt(text,key):
+    invert_key= generate_invert_key(key)
+    validate_decryption_input(text)
+    decrypted_text = ""
+    key_len = len(invert_key)
+    text_len=len(text)
+    if(text_len%key_len!=0):
+        raise ValueError("Input length for decryption must be a multiple of 2 ")
+    for i in range(text_len//key_len):
+        vector = [ord(text[i*key_len+j]) - first_character for j in range(key_len)]
+        for p in range(key_len):
+         result=0
+         for k in range(key_len):
+            result+=vector[k] * invert_key[p][k]  
+         decrypted_text += chr((result%num_of_characters)+first_character)
+    decrypted_text=decrypted_text.replace("@", "")
+
+    return decrypted_text
+    
 key_text = input("Enter the text to generate the key: ")
 key = generate_key(key_text)
 if key is not None :
     text = input("Enter the text to encrypt: ").upper().replace(" ", "")
     encrypted_text = encrypt(text, key)
     print("Encrypted text: ", encrypted_text)
-    # decrypted_text = decrypt(encrypted_text, key)
-    # print("Decrypted text: ", decrypted_text)
+    text = input("Enter the text to decrypt: ").upper().replace(" ", "")
+    decrypted_text = decrypt(text, key)
+    print("Decrypted text: ", decrypted_text)
